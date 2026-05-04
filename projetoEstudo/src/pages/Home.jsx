c0b1f030c66898cea6cb537f1eacacc7396f0cd9
Gian Warmling <130003869+GianWarmling@users.noreply.github.com>
2026-05-04 16:43:06 -0300

    Criando a conexão entre front e back

--- projetoEstudo/src/pages/Home.jsx
+++ projetoEstudo/src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
-import products from "../data/products";
 
 function Home() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://localhost:7111/api/products")
+        .then(res => res.json())
+        .then(data => setProducts(data))
+        .catch(err => console.error("Erro: ", err));
+    }, []);
+
     return ( 
         <div>
             <h1>Bem-vinda à nossa loja</h1>
